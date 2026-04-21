3630fabcca969e75b68606c9f380d924df37394e
satyamshrivastav955-dotcom <satyamshrivastav955@gmail.com>
2026-04-21 22:09:34 +0530

    Merge local scanner fixes with remote changes

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'add_product_screen.dart';
